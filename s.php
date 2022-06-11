39edb205928f0759ac5cda619fdcd5e567b2880e
mrfakename <me@mrfake.name>
2022-06-11 13:36:26 -0700

    Update s.php

--- s.php
+++ s.php
@@ -1,4 +1,5 @@
 <?php
+header('Content-type: text/plain');
 include 'init.php';
 function generateRandomString($length) {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyz';
@@ -26,17 +27,43 @@ if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
 } else {
     $ip = $_SERVER['REMOTE_ADDR'];
 }
+if (!empty($_GET['a'])) {
+    $alias = strtolower($_GET['a']);
+    if ((strlen($alias) < 3) || (strlen($alias) > 15)) {
+        die('Your alias must be between 3 and 15 characters long!');
+    }
+}
+if ((strtolower($_GET['a']) == 'privacy') || (strtolower($_GET['a']) == 'terms')) {
+    die('Sorry, your alias is reserved.');
+}
 if (!empty($_GET['u'])) {
     if (substr($_GET['u'], 0, strlen($shorturl)) === $shorturl) {
         die('Oops, you can\'t shorten that URL!');
     }
     if (filter_var($_GET['u'], FILTER_VALIDATE_URL)) {
-        $id = getID();
-        $stmt = $conn->prepare("INSERT INTO link (alias, url, ip) VALUES (?, ?, ?)");
-        $url = ($_GET['u']);
-        $stmt->bind_param('sss', $id, $url, $ip);
-        $stmt->execute();
-        echo $shorturl . $id;
+        if (!isset($alias)) {
+            $id = getID();
+            $stmt = $conn->prepare("INSERT INTO link (alias, url, ip) VALUES (?, ?, ?)");
+            $url = ($_GET['u']);
+            $stmt->bind_param('sss', $id, $url, $ip);
+            $stmt->execute();
+            echo $shorturl . $id;
+        } else {
+            $stmtcheckalias = $conn->prepare('SELECT * FROM link WHERE alias = ?');
+            $stmtcheckalias->bind_param('s', $alias);
+            $stmtcheckalias->execute();
+            $rescheck0 = $stmtcheckalias->get_result();
+            if (mysqli_num_rows($rescheck0) == 0) {
+                $id = $alias;
+                $stmt = $conn->prepare("INSERT INTO link (alias, url, ip) VALUES (?, ?, ?)");
+                $url = ($_GET['u']);
+                $stmt->bind_param('sss', $id, $url, $ip);
+                $stmt->execute();
+                echo $shorturl . $id;
+            } else {
+                die('Sorry, that alias is already taken!');
+            }
+        }
     } else {
         die("Error, invalid URL!");
     }
