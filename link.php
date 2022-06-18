9ebb35ab05b3bcbe109a68757c66325ae3b0de17
mrfakename <me@mrfake.name>
2022-06-18 08:48:54 -0700

    Update link.php
    fix critical issue where code echoing IP address

--- link.php
+++ link.php
@@ -4,11 +4,11 @@ if (empty($_GET['id'])) {
     die("Error, no link passed!");
 }
 if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-    echo $ip = $_SERVER['HTTP_CLIENT_IP'];
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
 } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-    echo $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
 } else {
-    echo $ip = $_SERVER['REMOTE_ADDR'];
+    $ip = $_SERVER['REMOTE_ADDR'];
 }
 $stmt = $conn->prepare('SELECT * FROM link WHERE alias = ?');
 $stmt->bind_param('s', $_GET['id']);
